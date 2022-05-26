62d286de6119f180e47390decd28c996cf929f1e
Tiago Giannoni Buso <tiagogb_2@hotmail.com>
2022-05-26 15:53:33 -0300

    - correção erro

--- CasaPopularTest/Startup.cs
+++ CasaPopularTest/Startup.cs
@@ -5,7 +5,8 @@ using Microsoft.Extensions.DependencyInjection;
 namespace CasaPopularTest
 {
     public class Startup
-    {        public void ConfigureServices(IServiceCollection services)
+    {   
+        public void ConfigureServices(IServiceCollection services)
         {
             services.AddTransient<IRanking, Ranking>();
             services.AddTransient<IPontuacaoServico, PontuacaoServico>();
